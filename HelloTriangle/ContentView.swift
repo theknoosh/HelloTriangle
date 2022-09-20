c862eeb697d2c70c5dc035d39f9a343ee8e3a621
Darrell Payne <darrellpayne@earthlink.net>
2022-09-19 23:29:09 -0700

    Initial Commit

---
+++ HelloTriangle/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HelloTriangle
+//
+//  Created by DARRELL A PAYNE on 9/19/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HelloTriangle/HelloTriangleApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloTriangleApp.swift
+//  HelloTriangle
+//
+//  Created by DARRELL A PAYNE on 9/19/22.
+//
+
+import SwiftUI
+
+@main
+struct HelloTriangleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
